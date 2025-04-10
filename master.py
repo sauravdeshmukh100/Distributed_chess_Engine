aacc4a9a17b7621094f4d18be6303cdb7f272208
Saurav Deshmukh <saurav.deshmukh20@vit.edu>
2025-04-10 15:08:03 +0530

    added pawn promotion

--- master.py
+++ master.py
@@ -1,36 +1,36 @@
-# --- master.py ---
-from mpi4py import MPI
-import chess
-from utils import serialize, deserialize, log
+# # --- master.py ---
+# from mpi4py import MPI
+# import chess
+# from utils import serialize, deserialize, log
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
+# comm = MPI.COMM_WORLD
+# size = comm.Get_size()
 
 
-def master_process():
-    board = chess.Board()
-    depth = 3
+# def master_process():
+#     board = chess.Board()
+#     depth = 3
 
-    legal_moves = list(board.legal_moves)
-    log(f"Distributing {len(legal_moves)} moves to {size-1} workers")
+#     legal_moves = list(board.legal_moves)
+#     log(f"Distributing {len(legal_moves)} moves to {size-1} workers")
 
-    for i, move in enumerate(legal_moves):
-        board.push(move)
-        task = serialize((board.fen(), depth - 1))
-        dest = (i % (size - 1)) + 1
-        comm.send(task, dest=dest)
-        log(f"Sent task to worker {dest}: {move}")
-        board.pop()
+#     for i, move in enumerate(legal_moves):
+#         board.push(move)
+#         task = serialize((board.fen(), depth - 1))
+#         dest = (i % (size - 1)) + 1
+#         comm.send(task, dest=dest)
+#         log(f"Sent task to worker {dest}: {move}")
+#         board.pop()
 
-    results = []
-    for i in range(len(legal_moves)):
-        result = comm.recv(source=MPI.ANY_SOURCE)
-        log(f"Received result: {result}")
-        results.append(deserialize(result))
+#     results = []
+#     for i in range(len(legal_moves)):
+#         result = comm.recv(source=MPI.ANY_SOURCE)
+#         log(f"Received result: {result}")
+#         results.append(deserialize(result))
 
-    best_score = max(results, key=lambda x: x[0])
-    log(f"Best move found: {best_score}")
+#     best_score = max(results, key=lambda x: x[0])
+#     log(f"Best move found: {best_score}")
 
-    for i in range(1, size):
-        comm.send("STOP", dest=i)
-        log(f"Sent STOP to worker {i}")
+#     for i in range(1, size):
+#         comm.send("STOP", dest=i)
+#         log(f"Sent STOP to worker {i}")
